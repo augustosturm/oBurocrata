af72615bcd0dd831d7281d48f56cba36270b2936
Augusto Sturm <augustoarnoldsturm@gmail.com>
2026-08-18 16:05:45 -0300

    Add Documento equals and hash methods

--- src/estudantes/entidades/Documento.java
+++ src/estudantes/entidades/Documento.java
@@ -1,6 +1,7 @@
 package estudantes.entidades;
 
 import professor.entidades.CodigoCurso;
+import java.util.Objects;
 
 /**
  * Classe que representa um documento genérico.
@@ -13,10 +14,33 @@ public abstract class Documento {
     private String criador;
     private CodigoCurso codigoCurso;
     private int paginas;
+    /**
+     * Construtor da classe Documento.
+     * 
+     * @param criador Nome do criador do documento
+     * @param codigoCurso Código do curso associado ao documento
+     * @param paginas Número de páginas do documento
+     */
     
     public Documento(String criador, CodigoCurso codigoCurso, int paginas){
         this.criador = criador;
         this.codigoCurso = codigoCurso;
         this.paginas = paginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Documento documento = (Documento) o;
+        return paginas == documento.paginas &&
+               Objects.equals(criador, documento.criador) &&
+               Objects.equals(codigoCurso, documento.codigoCurso);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(criador, codigoCurso, paginas);
     }
 }
