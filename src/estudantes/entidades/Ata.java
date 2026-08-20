237dd931af72e7eb2dc29b59697f6336bd1ae6db
Augusto Sturm <augustoarnoldsturm@gmail.com>
2026-08-20 11:41:06 -0300

    Add DocumentoAcademico class and fix hash and equals

--- src/estudantes/entidades/Ata.java
+++ src/estudantes/entidades/Ata.java
@@ -5,30 +5,55 @@ import java.util.Objects;
 
 import professor.entidades.CodigoCurso;
 
+/**
+ * Classe que representa uma ata de aula.
+ * <br><br>
+ *
+ * @author coloque os nomes dos autores aqui
+ */
 public class Ata extends Documento{
     private int numero;
     private String texto;
     private String[] presentes;
 
-    public Ata(String criador, CodigoCurso codigoCurso, int paginas) {
+    /**
+     * Construtor da classe Ata.
+     *
+     * @param criador Nome do criador do documento
+     * @param codigoCurso Código do curso associado ao documento
+     * @param paginas Número de páginas do documento
+     * @param numero Número da ata
+     * @param texto Texto da ata
+     * @param presentes Vetor com os nomes dos presentes na aula
+     */
+    public Ata(String criador, CodigoCurso codigoCurso, int paginas, int numero, String texto, String[] presentes) {
         super(criador, codigoCurso, paginas);
-        //TODO Auto-generated constructor stub
+        this.numero = numero;
+        this.texto = texto;
+        this.presentes = presentes;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Arrays.hashCode(presentes);
+		result = prime * result + Objects.hash(numero, texto);
+		return result;
+	}
 
-        Ata ata = (Ata) o;
-        return numero == ata.numero &&
-               Objects.equals(texto, ata.texto) &&
-               Arrays.equals(presentes, ata.presentes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), numero, texto, Arrays.hashCode(presentes));
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ata other = (Ata) obj;
+		return numero == other.numero && Objects.equals(texto, other.texto)
+				&& Arrays.equals(presentes, other.presentes);
+	}
 }
